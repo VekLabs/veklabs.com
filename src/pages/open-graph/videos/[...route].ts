c175ea8f53866c7c3bcb20d9fdd71ca7ff9d5653
Evan Hennessy <git@hennessyevan.com>
2025-03-06 11:51:25 -0500

    add dynamic og images to videos,reports

---
+++ src/pages/open-graph/videos/[...route].ts
@@ -0,0 +1,41 @@
+import { OGImageRoute } from "astro-og-canvas"
+import { getCollection, type CollectionEntry } from "astro:content"
+
+const collectionEntries = await getCollection("videos")
+const pages = Object.fromEntries(
+  collectionEntries.map(({ slug, data }) => [slug, data]),
+)
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  param: "route",
+
+  pages,
+
+  getSlug(path) {
+    return path
+  },
+
+  // For each page, this callback will be used to customize the OpenGraph image.
+  getImageOptions: async (path, page: CollectionEntry<"videos">["data"]) => ({
+    title: page.title,
+    description: "Video by Vek Labs".toLocaleUpperCase(),
+    bgImage: {
+      path: `./src/images/${page.image.src.match(/\/images\/(.*)/)![1].replace(/\?.*/, "")}`,
+      fit: "cover",
+    },
+    font: {
+      title: { families: ["Poppins"], weight: "Bold" },
+      description: { families: ["Poppins"], weight: "Normal" },
+    },
+    fonts: [
+      "https://cdn.jsdelivr.net/fontsource/fonts/poppins@latest/latin-400-normal.ttf",
+      "https://cdn.jsdelivr.net/fontsource/fonts/poppins@latest/latin-500-normal.ttf",
+      "https://cdn.jsdelivr.net/fontsource/fonts/poppins@latest/latin-600-normal.ttf",
+    ],
+    format: "WEBP",
+    logo: {
+      size: [100],
+      path: "./src/images/logo-white.png",
+    },
+  }),
+})
